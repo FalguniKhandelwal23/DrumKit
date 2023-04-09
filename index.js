31af8873a29adb2c66f5c8e2ececa9ad785f3ba0
Falguni Khandelwal <125128719+FalguniKhandelwal23@users.noreply.github.com>
2023-04-09 23:02:20 +0530

    Update index.js

--- index.js
+++ index.js
@@ -26,37 +26,37 @@ document.addEventListener("keypress", function(event) {
 function makeSound(key) {
 
   switch (key) {
-    case "W":
+    case "w":
       var tom1 = new Audio("sounds/tom-1.mp3");
       tom1.play();
       break;
 
-    case "A":
+    case "a":
       var tom2 = new Audio("sounds/tom-2.mp3");
       tom2.play();
       break;
 
-    case "S":
+    case "s":
       var tom3 = new Audio('sounds/tom-3.mp3');
       tom3.play();
       break;
 
-    case "D":
+    case "d":
       var tom4 = new Audio('sounds/tom-4.mp3');
       tom4.play();
       break;
 
-    case "J":
+    case "j":
       var snare = new Audio('sounds/snare.mp3');
       snare.play();
       break;
 
-    case "K":
+    case "k":
       var crash = new Audio('sounds/crash.mp3');
       crash.play();
       break;
 
-    case "L":
+    case "l":
       var kick = new Audio('sounds/kick-bass.mp3');
       kick.play();
       break;
